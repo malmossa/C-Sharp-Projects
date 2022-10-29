2502c3d05d105d5866a09921bac1bc666c1c097d
Mansor Almossa <malmossa@gmail.com>
2022-10-29 08:11:25 -0700

    completed

--- MathGame/MathGame/Program.cs
+++ MathGame/MathGame/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please type your name");
+
+string name = Console.ReadLine();
+DateTime date = DateTime.Now;
+
+
+Console.WriteLine("-------------------------");
+Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your math game.\n");
+Console.WriteLine($@"What game would you like to play today? Choose from the option below:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+Console.WriteLine("--------------------------------------");
+
+
+Console.ReadLine();
+
+
+
