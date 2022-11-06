f4ec4a52a11ea231b58862e220279a88ba5068d8
Mansor Almossa <malmossa@gmail.com>
2022-11-06 10:00:36 -0800

    completed

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -6,25 +6,12 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            int x; // declaration
-            x = 123; // initialization
+            String a = "true";
+            bool b = Convert.ToBoolean(a);
 
-            int y = 321; // declaration + initialization
+            Console.WriteLine(b);
+            Console.WriteLine(b.GetType());
 
-            int age = 21; // whole integer
-            double height = 5.3; // decimal number
-            bool alive = true; // true or false
-            char symbol = '@'; // single quotes
-            String name = "Mansor";
-            
-            
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine("Hello, " + name);
-            Console.WriteLine("Your age is " + age);
-            Console.WriteLine("Your height is " + height + " foot");
-            Console.WriteLine("Are you alive? " + alive);
-            Console.WriteLine("Your symbol is: " + symbol);
 
             Console.ReadKey();
         }
