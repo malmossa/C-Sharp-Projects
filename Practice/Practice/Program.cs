6f19aa76ef20c9ef5b5aba1ae0fe817413f0f0de
Mansor Almossa <malmossa@gmail.com>
2022-11-17 07:50:49 -0800

    completed

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -8,59 +8,28 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            List<string> food = new List<string>();
+            Car car = new Car(200);
 
-            // Add method
-            food.Add("pizza");
-            food.Add("Hamburger");
-            food.Add("Hotdog");
-            food.Add("Fries");
-
-            // Remove method
-            food.Remove("Fries");
-
-            // Insert methos - index and item
-            food.Insert(0, "Sushi");
-
-            // Access the list
-            Console.WriteLine(food[0]);
-            Console.WriteLine(food[1]);
-            Console.WriteLine(food[2]);
-
-            // or with foreach loop
-            foreach(String item in food)
-            {
-                Console.WriteLine(item);
-            }
-
-            // Getting the list size with count property
-            Console.WriteLine(food.Count);
-
-            // Finding the index of an item
-            Console.WriteLine(food.IndexOf("Hotdog"));
-
-            // Finfing to see if an item is in the list with Contains methos
-            Console.WriteLine(food.Contains("Sushi"));
-
-            //to sort the list alphabetically
-            food.Sort();
-
-            // to sort in reverse
-            food.Reverse();
-
-            // to clear the list 
-            food.Clear();
-
-            // Convert the list to an array
-            String[] foodArray = food.ToArray();
-
-
-
+            car.Speed = 300;
 
             Console.ReadKey();
         }
 
+        class Car
+        {
+            private int speed;
 
+            public Car(int speed)
+            {
+                Speed = speed;
+            }
+
+            public int Speed
+            {
+                get { return speed; } 
+                set { speed = value; }
+            }
+        }
     }
 }
 
