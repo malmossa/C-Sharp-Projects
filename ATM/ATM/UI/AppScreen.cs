8e1a6ca66f6e03bf778b9ed19c3c464c81b82d81
Mansor Almossa <malmossa@gmail.com>
2022-11-26 09:46:08 -0800

    completed

--- ATM/ATM/UI/AppScreen.cs
+++ ATM/ATM/UI/AppScreen.cs
@@ -1,7 +1,22 @@
 ﻿
 namespace ATM.UI
 {
-    public static class AppScreen
+    public class AppScreen
     {
+        public static void Welcome()
+        {
+            Console.Title = "ATM APP";
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\n---------- Welcome to The ATM Program ----------\n\n");
+
+            // prompt the user to insert ATM card
+            Console.WriteLine("Please insert your ATM card");
+            Console.WriteLine("NOTE: This is a virtual ATM");
+
+            Utility.PressEnterToContinue();
+
+        }
+
     }
 }
