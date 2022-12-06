99d8daa98e3e566b6fffc7aaada03253523431b1
Mansor Almossa <malmossa@gmail.com>
2022-12-05 17:36:08 -0800

    completed

---
+++ MasterCourse/ConditionalHomework/ConditionalHomework/Program.cs
@@ -0,0 +1,21 @@
+﻿
+/* Create a console application that asks the user for their name.
+   Welcome me (Mansor) as professor or anyone else as student.
+   Make sure that "Mansor" also gets called professor. */
+
+Console.WriteLine("What is your name? ");
+
+string name = Console.ReadLine();
+
+if (name.ToLower() == "mansor")
+{
+    Console.WriteLine($"Welcome professor {name}.");
+} else
+{
+    Console.WriteLine($"Welcome student {name}.");
+}
+
+
+Console.ReadLine();
+
+
