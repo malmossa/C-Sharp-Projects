ee0b8455e63e4dd9f2632f2fc2a227e58520a193
Mansor Almossa <malmossa@gmail.com>
2022-12-06 07:33:11 -0800

    completed

---
+++ MasterCourse/DoLoopsHomework/DoLoopsHomework/Program.cs
@@ -0,0 +1,34 @@
+﻿
+/* Create a console application that asks the user for their name.
+   Welcome me (Mansor) as professor or anytone else as student.
+   Do this until the user types "exit" */
+
+bool runAgin = true;
+
+do
+{
+    Console.Write("What is you name: ");
+    string name = Console.ReadLine();
+
+    if(name.ToLower() == "mansor")
+    {
+        Console.WriteLine($"Welcome professor {name.ToUpper()}");
+    } else
+    {
+        Console.WriteLine($"Welcome student {name.ToUpper()}");
+    }
+
+    Console.Write("Do you want to exit? y / n: ");
+    string answer = Console.ReadLine();
+
+    if (answer.ToLower() == "y")
+    {
+        runAgin = false;
+        Console.WriteLine("GOODBUY!!");
+    }
+
+} while (runAgin);
+
+
+
+Console.ReadLine();
--- ToDoAPI/ToDoAPI/Models/ToDo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace toDoAPI.Models
-{
-    public class ToDo
-    {
-        public int Id { get; set; }
-        public string ToDoName {get; set;}
-    }
-}
