0a3d2f24509b77209005d19d2e941d3d8d9f78d9
Mansor Almossa <malmossa@gmail.com>
2022-12-11 14:35:54 -0800

    completed

---
+++ MasterCourse/ForLoopHomework/ForLoopHomework/Program.cs
@@ -0,0 +1,18 @@
+﻿
+/* Ask the user for a comma-separated list of first names (no spaces).
+   Loop through the array and print "Hello <name>" 
+   to the console fir each person.*/
+
+
+Console.WriteLine("Can you provide a list of first names seperated with a , :");
+
+List<string> firstNames = Console.ReadLine().Split(',').ToList();
+
+foreach (string name in firstNames)
+{
+    Console.WriteLine($"Hello {name}");
+}
+
+
+
+Console.ReadLine();
