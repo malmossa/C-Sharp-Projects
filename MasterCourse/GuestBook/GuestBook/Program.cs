a346f58f4e09e03bdff9e2529471b39ff17a0d3e
Mansor Almossa <malmossa@gmail.com>
2022-12-14 10:26:30 -0800

    completed

---
+++ MasterCourse/GuestBook/GuestBook/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Build a console Guest Book. Ask the user for their name and how
+   many are in their party.
+   Keep track of how many people are at the party.
+   At the end, print out the guest list and the total number of guests.*/
+
+using GuestBook;
+
+
+Console.WriteLine("*************************");
+Console.WriteLine("*      Guest Book       *");
+Console.WriteLine("*************************");
+
+GuestList.GetGuestNamesAndNumbers();
+
+
+Console.ReadLine();
