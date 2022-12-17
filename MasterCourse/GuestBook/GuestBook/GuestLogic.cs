5c0629c276a1bf99b770d61240c21427b2c35555
Mansor Almossa <malmossa@gmail.com>
2022-12-16 21:33:49 -0800

    still on it

---
+++ MasterCourse/GuestBook/GuestBook/GuestLogic.cs
@@ -0,0 +1,35 @@
+﻿
+namespace GuestBook
+{
+    public static class GuestLogic
+    {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Wolcome to the Guest Book App");
+            Console.WriteLine("*****************************\n");
+        }
+
+        public static string GetPartyName()
+        {
+            Console.Write("What is your Party/Group name: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+
+        public static int GetPartySize()
+        {
+            bool isValidNumber;
+            int output;
+            do
+            {
+                Console.Write("How many people are in your party: ");
+                string partySizeText = Console.ReadLine();
+
+                isValidNumber = int.TryParse(partySizeText, out output); 
+            } while (!isValidNumber);
+
+            return output;
+        }
+     }
+}
