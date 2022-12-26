5273188ca1f29795c40acc67adb17e29840cbb78
Mansor Almossa <malmossa@gmail.com>
2022-12-26 07:21:35 -0800

    completed

--- MasterCourse/Practice/Practice/Program.cs
+++ MasterCourse/Practice/Practice/Program.cs
@@ -1,38 +1,18 @@
 ﻿
-
 namespace Practice
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // initiate list from the PersonModel class 
-            List<PersonModel> users = new List<PersonModel>();
-
-            // create user 1 from the PersonModel class
-            PersonModel user = new PersonModel();
-            user.firstName = "Mansor";
-            // add the user to the list
-            users.Add(user);
-
-            // create user 2
-            user = new PersonModel();
-            user.firstName = "Nujood";
-            // add to the list
-            users.Add(user);
-
-            // print out the list items
-            foreach (PersonModel u in users)
+            printItems(10);
+        }
+        public static void printItems(int n)
+        {
+            for (int i = 0; i < n; i++)
             {
-                Console.WriteLine(u.firstName);
+                Console.WriteLine(i);
             }
-
-
-            
-
-
-
-            Console.ReadLine();
         }
     }
 }
