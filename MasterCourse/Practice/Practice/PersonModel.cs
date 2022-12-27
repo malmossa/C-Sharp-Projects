f48fb2326d5030bb8324ab7a338e337b9aa4350d
Mansor Almossa <malmossa@gmail.com>
2022-12-27 09:34:01 -0800

    still on it

--- MasterCourse/Practice/Practice/PersonModel.cs
+++ MasterCourse/Practice/Practice/PersonModel.cs
@@ -1,12 +1,48 @@
 ﻿
-
 namespace Practice
 {
     public class PersonModel
     {
-        // fields
-        public string firstName;
-        public string lastName;
-        public int age;
+        // use of auto properties, note it start with capital letter
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        // use of full properties, we use this if we neet to add some logic
+        private int _age;
+        private string _ssn;
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+
+            set
+            {
+                if (value > 0 && value < 120)
+                {
+                    _age = -value;
+                } else
+                {
+                    throw new ArgumentOutOfRangeException("Value", "Age needs to be in a valid range.");
+                }
+            }
+        }
+
+        public string SSN 
+        {
+            get 
+            { 
+                string output = "***-**-" + _ssn.Substring(_ssn.Length - 4);
+                return output;
+            }
+
+            set
+            {
+                _ssn= value;
+            }
+        }
     }
 }
--- MasterCourse/Practice/Practice/Program.cs
+++ MasterCourse/Practice/Practice/Program.cs
@@ -1,41 +1,33 @@
 ﻿
-
 namespace Practice
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // initiate list from the PersonModel class 
-            List<PersonModel> users = new List<PersonModel>();
+            PersonModel person= new PersonModel();
 
-            // create user 1 from the PersonModel class
-            PersonModel user = new PersonModel();
-            user.firstName = "Mansor";
-            // add the user to the list
-            users.Add(user);
+            person.FirstName = "Mansor";
+            person.LastName = "Almossa";
+            person.Age = 30;
+            person.SSN = "123-45-7890";
 
-            // create user 2
-            user = new PersonModel();
-            user.firstName = "Nujood";
-            // add to the list
-            users.Add(user);
-
-            // print out the list items
-            foreach (PersonModel u in users)
-            {
-                Console.WriteLine(u.firstName);
-            }
-
-
-            
-
+            Console.WriteLine(person.SSN);
 
 
             Console.ReadLine();
         }
     }
 }
+
+
+
+
+            
+
+
+
+           
 
 
 
