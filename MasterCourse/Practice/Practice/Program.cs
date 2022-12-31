e5bdc27407881d2b746dce0dbdcd87b22502ef3f
Mansor Almossa <malmossa@gmail.com>
2022-12-31 07:56:25 -0800

    completed

--- MasterCourse/Practice/Practice/Program.cs
+++ MasterCourse/Practice/Practice/Program.cs
@@ -1,18 +1,12 @@
-﻿
+﻿using System;
+
 namespace Practice
 {
     class Program
     {
         static void Main(string[] args)
         {
-            printItems(10);
-        }
-        public static void printItems(int n)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine(i);
-            }
+            Console.WriteLine("Hello");
         }
     }
 }
