2ee263a6139fca26c9ddb466d60966826676c598
Mansor Almossa <malmossa@gmail.com>
2024-02-10 10:04:57 -0800

    completed

---
+++ MarysCandyShop/MarysCandyShop/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarysCandyShop
+{
+    internal static class Configuration
+    {
+        internal static string docPath = @"C:\Users\malmo\C-Sharp-Projects\MarysCandyShop\MarysCandyShop\products.txt";
+    }
+}
---
+++ MarysCandyShop/MarysCandyShop/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace MarysCandyShop
+{
+    internal class Product
+    {
+        internal string Name;
+
+        public Product(string name)
+        {
+            Name = name;
+        }
+    }
+}
